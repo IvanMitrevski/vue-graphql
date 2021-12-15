320a18e2b63bfa5e4e1e19c24f31875661924fcd
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-12-15 14:42:32 +0100

    Query GraphQL Data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/rockets">Rockets</router-link>
     </div>
     <router-view/>
   </div>
@@ -28,5 +29,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+li {
+  list-style-type: none;
 }
 </style>
